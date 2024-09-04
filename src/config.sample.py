6022ffd32c20c2ddb1320886746f6594c275de95
AlanFromJapan <alanfromjapan@gmail.com>
2024-09-04 21:59:22 +0900

    Now filters for the fastest usual route and check if not longuer than usual

---
+++ src/config.sample.py
@@ -0,0 +1,10 @@
+conf = {
+    #the URL to connect to (Yahoo Japan in this case)
+    "url" : "https://transit.yahoo.co.jp/search/result?from=%E7%B6%BE%E7%80%AC&to=%E6%96%B0%E6%9D%BF%E6%A9%8B&fromgid=&togid=&flatlon=%2C%2C22499&tlatlon=%2C%2C22729&via=&viacode&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1",
+
+    #the longest expected minimum duration for the trip
+    "longest_expected_minimum_duration" : 45,
+    
+    #the expected number of transfers
+    "expected_transfers" : 2
+}
--- src/mmm.py
+++ src/mmm.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 from webutils import get_web_page_content
 import routes
 import re
+from config import conf
 
-URL="https://transit.yahoo.co.jp/search/result?from=%E7%B6%BE%E7%80%AC&to=%E6%96%B0%E6%9D%BF%E6%A9%8B&fromgid=&togid=&flatlon=%2C%2C22499&tlatlon=%2C%2C22729&via=&viacode&type=1&ticket=ic&expkind=1&userpass=1&ws=3&s=0&al=1&shin=1&ex=1&hb=1&lb=1&sr=1"
 
 RE_DEPART_END="""(?P<departure>[^→]+)→(?P<arrival>[^（]+)（(?P<duration>\d+)"""
 re_depart_end = re.compile(RE_DEPART_END)
@@ -12,21 +12,21 @@ re_depart_end = re.compile(RE_DEPART_END)
 def get_metro_status():
     result = []
 
-    html_body = get_web_page_content(URL)
+    html_body = get_web_page_content(conf["url"])
 
     soup = BeautifulSoup(html_body.decode("utf-8"), 'html.parser')
 
-    # with open("output.html", "w") as file:
-    #     file.write(str(response.decode("utf-8"),))
-
+    #the DOM is HEAVILY modified by javascript, so we need to find the right object. So parse whatever BS4 gives us, not what you see in the browser.
+    #use the summary class to get the route list
     routelist = soup.find_all("ul", class_="summary")
 
     for route in routelist:
         
         m = re_depart_end.match(route.find("li", class_="time").text)
+
         r = routes.Route(
-            duration= m.group("duration"),
-            transfer_count= re.findall(r"\d+", route.find("li", class_="transfer").text)[0],
+            duration= int(m.group("duration")),
+            transfer_count= int(re.findall(r"\d+", route.find("li", class_="transfer").text)[0]),
             departure= m.group("departure"),
             arrival= m.group("arrival")
         )
@@ -40,12 +40,34 @@ def get_metro_status():
 
     return result
 
+
+def check_for_delays(routes):
+    #basically in my case if it takes me more than 45 minutes to bring the kids to school, there's a problem and I need to know about it BEFORE I leave the house.
+    delay=False
+    fastest_route = None
+
+    longest_expected_minimum_duration = conf["longest_expected_minimum_duration"]
+    expected_transfers = conf["expected_transfers"]
+
+    for route in routes:
+        if route.transfer_count == expected_transfers and '早' in route.flags:
+            fastest_route = route
+            if route.duration > longest_expected_minimum_duration:
+                delay= True
+
+    return fastest_route, delay
+
+
+
 def main():
     metro_status = get_metro_status()
 
-    for route in metro_status:
-        print(route)
-        print(" ")
+    fastest_route, delay = check_for_delays(metro_status)
+    print (fastest_route)
+    if delay:
+        print("There are delays in the metro system")
+    else:
+        print("Metro system is running smoothly")
 
 if __name__ == "__main__":
     main()
